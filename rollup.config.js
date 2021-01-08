9e35cf209eb3115cef8098e53e14c3c82189652b
Manuel Juan Fosela Águila <manufosela@gmail.com>
2021-01-08 23:06:39 +0100

    add postcss to minify css

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,46 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import copy from 'rollup-plugin-copy';
+import multiInput from 'rollup-plugin-multi-input';
 import { terser } from 'rollup-plugin-terser';
+import replaceHtmlVars from 'rollup-plugin-replace-html-vars';
 
 export default {
   preserveSymlinks: true,
-	input: ['animation-protonimg.js'],
-	output: {
-		file: 'dist/animation-protonimg.js',
+  input: ['./animation-protonimg.js'],
+  output: {
+    dir: './dist/js',
     format: 'es',
-    sourcemap: true
+    sourcemap: true,
   },
   plugins: [
     resolve(),
     babel(),
-    terser()
-  ]
+    terser({
+      output: {
+        comments: false,
+      },
+    }),
+    copy({
+      targets: [{
+        src: './animation-protonimg.js',
+        dest: './dist/js',
+      },{
+        src: './demo/css/styles.css',
+        dest: './dist/css',
+      },{
+        src: './demo/img/*',
+        dest: './dist/img',
+      },{
+        src: './demo/index.html',
+        dest: './dist',
+      }],
+    }),
+    replaceHtmlVars({
+      files: './demo/index.html',
+      from: '<script type="module" src="..',
+      to: '<script type="module" src="js',
+    }),
+  ],
 };
+
