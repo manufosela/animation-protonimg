6373a37b2b91c4fcbc13aa2cebfa42a18ec2456b
Manuel Juan Fosela Águila <manufosela@gmail.com>
2021-01-09 13:12:46 +0100

    improvment devDependencies version

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import copy from 'rollup-plugin-copy';
-import multiInput from 'rollup-plugin-multi-input';
 import { terser } from 'rollup-plugin-terser';
 import replaceHtmlVars from 'rollup-plugin-replace-html-vars';
 
